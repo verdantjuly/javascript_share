23c6d862d12a229473b78eaf64cdfd8bf60f6ebb
dayounglee <verdantjuly@kakao.com>
2024-09-26 15:39:26 +0900

    [Fix] 계산기 매개변수 위치 변경

--- 06.quiz.calculator.js
+++ 06.quiz.calculator.js
@@ -1,7 +1,7 @@
 // 두 수와 연산자(String형)를 입력받아서
 // 사칙연산 후 결과를 반환하는 함수를 만드시오.
 
-function calculator(num1, num2, operator) {
+function calculator(num1, operator, num2) {
   let result = 0;
   switch (operator) {
     case "+":
@@ -26,4 +26,4 @@ function calculator(num1, num2, operator) {
   return result;
 }
 
-calculator(2, 4, "*");
+calculator(2, "*", 4);
