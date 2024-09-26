f7fa119a1e078c4f9915de6460265e2edd657315
dayounglee <verdantjuly@kakao.com>
2024-09-26 14:43:32 +0900

    [Fix] 학점 계산기  ;값을 반환하는 함수로 변경

--- 06.quiz.scoreCalculatorFunc.js
+++ 06.quiz.scoreCalculatorFunc.js
@@ -2,25 +2,24 @@
 // 학점(A,B~F)을 반환하는 함수를 정의하고 여러번 호출하시오.
 
 function scoreCalculator(score) {
-  console.log("주어진 점수에 따른 학점을 출력합니다.");
   if (score >= 90) {
-    console.log("A");
+    return "A";
   } else if (score >= 80) {
-    console.log("B");
+    return "B";
   } else if (score >= 70) {
-    console.log("C");
+    return "C";
   } else if (score >= 60) {
-    console.log("D");
+    return "D";
   } else if (score >= 50) {
-    console.log("E");
+    return "E";
   } else {
-    console.log("F");
+    return "F";
   }
 }
 
-scoreCalculator(90);
-scoreCalculator(80);
-scoreCalculator(70);
-scoreCalculator(60);
-scoreCalculator(50);
-scoreCalculator(40);
+console.log(scoreCalculator(90));
+console.log(scoreCalculator(80));
+console.log(scoreCalculator(70));
+console.log(scoreCalculator(60));
+console.log(scoreCalculator(50));
+console.log(scoreCalculator(40));
