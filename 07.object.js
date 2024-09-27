281d999da04cbd7261c6b8d4bcc2f29725cc315e
dayounglee <verdantjuly@kakao.com>
2024-09-27 15:04:26 +0900

    [Feat] Object

---
+++ 07.object.js
@@ -0,0 +1,12 @@
+// Object 객체
+
+// 데이터와 기능을 한데 묶어 관리할 수 있는 중요한 자료형
+// 객체는 키(Key)와 값(Value)의 쌍으로 구성
+// 키(Key)와 값(Value)를 Object의 속성이라고 함
+// 함수(Function)을 Value로 가질 수 있음
+// 객체 내의 정의된 함수를 메서드(Method)라고 함
+
+// 형태
+// const obj = {key : "value"}
+
+// 사전과 비슷하다고 생각하면 된다. key를 알면 value를 뽑아낼 수 있다.
