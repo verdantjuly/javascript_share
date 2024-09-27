663ca3d26781482ad2824aa69014d625b9d48264
dayounglee <verdantjuly@kakao.com>
2024-09-27 16:43:57 +0900

    [Feat] Vehicle 퀴즈

--- 07.quiz.vehicle.js
+++ 07.quiz.vehicle.js
@@ -1,3 +1,10 @@
+// Quiz
+// • 운송수단을 의미하는 Vehicle 클래스를 정의하라.
+// • 속도를 관리하는 speed라는 속성을 정의하라.
+// • speed를 10 증가하는 메쏘드를 정의하라.
+// • speed를 10 감소하는 메쏘드를 정의하라.
+// • 현재 속도를 표기하는 info 메쏘드를 정의하라.
+
 class Vehicle {
   constructor(speed) {
     this.speed = speed;
