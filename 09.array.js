250d51a6265361a574c8e4584a65f1ed18b52c34
dayounglee <verdantjuly@kakao.com>
2024-09-30 12:25:28 +0900

    [Feat] array

--- 09.array.js
+++ 09.array.js
@@ -6,7 +6,12 @@ const numbers = [1, 2, 3, 4, 5]; // 리터럴 방식
 console.log(numbers[0]); // index는 0번부터 시작
 
 let array = new Array(2); // size를 2로 정의
-console.log(array); // [undefined, undefined]
+console.log(array); // <2 empty items>
+console.log(array[0]); // undefined
+console.log(array[1]); // undefined
 
 array = new Array(1, 2, 3, 4, 5);
 console.log(array);
+
+// length - 요소의 개수
+console.log(numbers.length);
