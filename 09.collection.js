bd482f3fceb62ae0a4a2be903e3aa476fdf53773
dayounglee <verdantjuly@kakao.com>
2024-09-30 12:14:30 +0900

    [Feat] Collection Type

--- 09.collection.js
+++ 09.collection.js
@@ -1,6 +1,10 @@
 // Collection Type (집단 자료형)
 // 서로 연관이 있는 데이터끼리 모아서 관리
 
-// 배열 (Array) : 순서 O
+// 배열 (Array) : 순서 O, index 0번부터 시작
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers[0]); // index는 0번부터 시작
+
 // 집합 (Set) : 중복 X
-// 맵 (Map) : Key - Value
+
+// 맵 (Map) : Key - Value, 데이터 중심
