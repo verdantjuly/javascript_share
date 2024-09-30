61e04b19a52e60eec799ed2679848b2dc0ccf4d6
dayounglee <verdantjuly@kakao.com>
2024-09-30 12:37:27 +0900

    [Feat] array

--- 09.array.js
+++ 09.array.js
@@ -5,7 +5,7 @@
 const numbers = [1, 2, 3, 4, 5]; // 리터럴 방식
 console.log(numbers[0]); // index는 0번부터 시작
 
-let array = new Array(2); // size를 2로 정의
+let array = new Array(2); // size를 2로 정의 , 생성자 클래스 방식
 console.log(array); // <2 empty items>
 console.log(array[0]); // undefined
 console.log(array[1]); // undefined
@@ -27,3 +27,13 @@ console.log(numbers.length, numbers);
 // item 삭제
 delete numbers[1]; // undefined가 된다.
 console.log(numbers.length, numbers);
+
+// Array 활용
+let bts1 = ["진", "슈가", "제이홉", "RM"];
+console.log(bts1);
+let bts2 = new Array("지민", "뷔", "정국");
+console.log(bts1.indexOf("슈가")); // 순서 알려주는 메서드, 숫자형 반환
+console.log(bts2.indexOf("슈가"));
+
+console.log(bts1.includes("RM")); // 포함되어 있는지 아닌지 알려주는 메서드, 불리언 반환
+console.log(bts2.includes("지민"));
