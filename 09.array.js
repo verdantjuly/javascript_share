22aef60ef9f1511d81443665d383ca9a0b51947e
dayounglee <verdantjuly@kakao.com>
2024-09-30 12:23:21 +0900

    [Feat] Collection Type

--- 09.array.js
+++ 09.array.js
@@ -2,7 +2,7 @@
 // 서로 연관이 있는 데이터끼리 모아서 관리
 
 // 배열 (Array) : 순서 O, index 0번부터 시작
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5]; // 리터럴 방식
 console.log(numbers[0]); // index는 0번부터 시작
 
 let array = new Array(2); // size를 2로 정의
