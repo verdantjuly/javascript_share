9059cdd7bf10d237c1c900c5a5defa6ab5a9ef08
dayounglee <verdantjuly@kakao.com>
2024-10-04 11:36:23 +0900

    [Feat] promise

---
+++ 16.promise.js
@@ -0,0 +1,62 @@
+// Promise : 약속
+// 콜백의 단점을 보완하기 위해 만들어진 비동기 처리 객체
+
+// 대기(Pending) : 미완료
+// 이행(fulfilled) : 완료
+// 거부(reject) : 실패
+
+// 성공 > reolve > .then
+// 실패 > reject > .catch
+
+// Promise는 체이닝이 된다.
+
+const db = [];
+
+function saveDB(user) {
+  const oldDBlength = db.length;
+  db.push(user);
+  console.log(`save ${user.name} to DB `);
+  return new Promise((resolve, reject) => {
+    if (db.length > oldDBlength) {
+      resolve(user);
+    } else {
+      reject(new Error("Save DB Error"));
+    }
+  });
+}
+
+function sendEmail(user) {
+  console.log(`send email to ${user.email}`);
+  return new Promise((resolve) => {
+    resolve(user);
+  });
+}
+
+function getResult(user) {
+  return new Promise((resolve) => {
+    resolve(`success register : ${user.name}`);
+  });
+}
+
+function registerByPromise(user) {
+  const result = saveDB(user)
+    .then(sendEmail)
+    .then(getResult)
+    .catch((error) => {
+      new Error(error);
+    })
+    .finally(() => {
+      console.log("====== 완료! ======");
+    });
+
+  console.log("Promise pending... ", result);
+  return result;
+}
+
+const user = {
+  name: "유재석",
+  email: "you@jae.com",
+};
+
+const finalResult = registerByPromise(user);
+finalResult.then(console.log);
